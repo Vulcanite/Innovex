7b2028fce5c33f3a6507514f3d53f3df6f5c1168
hisenberggg <hknocks8@gmail.com>
2021-04-19 00:39:23 +0530

    project model added

--- website1/admin.py
+++ website1/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
+from website1.models import Project
 # Register your models here.
+
+admin.site.register(Project)
--- website1/models.py
+++ website1/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
+DEPT_CHOICES = (
+    ('it','IT'),
+    ('comps', 'COMPS'),
+    ('extc','EXTC'),
+    ('mech','MECH'),
+)
+
+CATEGORY_CHOICES = (
+    ('pro_dev','PRODUCT DEVELOPMENT'),
+    ('research', 'RESEARCH BASED'),
+    ('env_sust','ENVIRONMENT SUSTAINABILITY'),
+)
+
 # Create your models here.
+class Project(models.Model):
+    proj_id = models.AutoField(primary_key=True)
+    proj_title = models.CharField(max_length=100)
+    proj_category = models.CharField(max_length=50, choices=CATEGORY_CHOICES, default='pro_dev')
+    dept = models.CharField(max_length=6, choices=DEPT_CHOICES, default='it')
+    mem1_name = models.CharField(max_length=100)
+    mem2_name = models.CharField(max_length=100)
+    mem3_name = models.CharField(max_length=100)
+    mem1_linkedin = models.CharField(max_length=100)
+    mem2_linkedin = models.CharField(max_length=100)
+    mem3_linkedin = models.CharField(max_length=100)
+    youtube_link = models.CharField(max_length=11)
+    proj_description = models.TextField()
