ec7132073728caf81e059c3f8c3d72430296c690
hisenberggg <hknocks8@gmail.com>
2021-04-19 13:57:27 +0530

    project model and dynamic rendering
    Issue: linkedin link opening with website prefix

--- website1/models.py
+++ website1/models.py
@@ -8,16 +8,16 @@ DEPT_CHOICES = (
 )
 
 CATEGORY_CHOICES = (
-    ('pro_dev','PRODUCT DEVELOPMENT'),
-    ('research', 'RESEARCH BASED'),
-    ('env_sust','ENVIRONMENT SUSTAINABILITY'),
+    ('PRODUCT DEVELOPMENT','PRODUCT DEVELOPMENT'),
+    ('RESEARCH BASED', 'RESEARCH BASED'),
+    ('ENVIRONMENT SUSTAINABILITY','ENVIRONMENT SUSTAINABILITY'),
 )
 
 # Create your models here.
 class Project(models.Model):
     proj_id = models.AutoField(primary_key=True)
     proj_title = models.CharField(max_length=100)
-    proj_category = models.CharField(max_length=50, choices=CATEGORY_CHOICES, default='pro_dev')
+    proj_category = models.CharField(max_length=50, choices=CATEGORY_CHOICES, default='PRODUCT DEVELOPMENT')
     dept = models.CharField(max_length=6, choices=DEPT_CHOICES, default='it')
     mem1_name = models.CharField(max_length=100)
     mem2_name = models.CharField(max_length=100)
--- website1/views.py
+++ website1/views.py
@@ -5,22 +5,86 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.models import User, Group
 from django.db import IntegrityError
 from website1.decorators import unauthenticated_user
-
+from website1.models import Project
 
 def itdept(request):
-    return render(request, "website1/it.html")
+    datait=Project.objects.filter(dept='it')
+    datait_rb=Project.objects.filter(dept='it').filter(proj_category='RESEARCH BASED')
+    datait_pd=Project.objects.filter(dept='it').filter(proj_category='PRODUCT DEVELOPMENT')
+    datait_env=Project.objects.filter(dept='it').filter(proj_category='ENVIRONMENT SUSTAINABILITY')
+    # data=Project.objects.all()
+
+    # print(datait.values())
+    # print(datait_rb.values())
+    # print(datait_pd.values())
+    # print(datait_env.values())
+    context={
+    'datait':datait,
+    'datait_rb':datait_rb,
+    'datait_pd':datait_pd,
+    'datait_env':datait_env
+    }
+    return render(request, "website1/it.html",context)
 
 
 def compsdept(request):
-    return render(request, "website1/comps.html")
+    datacomps=Project.objects.filter(dept='comps')
+    datacomps_rb=Project.objects.filter(dept='comps').filter(proj_category='RESEARCH BASED')
+    datacomps_pd=Project.objects.filter(dept='comps').filter(proj_category='PRODUCT DEVELOPMENT')
+    datacomps_env=Project.objects.filter(dept='comps').filter(proj_category='ENVIRONMENT SUSTAINABILITY')
+    # data=Project.objects.all()
+
+    # print(datacomps.values())
+    # print(datacomps_rb.values())
+    # print(datacomps_pd.values())
+    # print(datacomps_env.values())
+    context={
+    'datacomps':datacomps,
+    'datacomps_rb':datacomps_rb,
+    'datacomps_pd':datacomps_pd,
+    'datacomps_env':datacomps_env
+    }
+    return render(request, "website1/comps.html",context)
 
 
 def mechdept(request):
-    return render(request, "website1/mech.html")
+    datamech=Project.objects.filter(dept='mech')
+    datamech_rb=Project.objects.filter(dept='mech').filter(proj_category='RESEARCH BASED')
+    datamech_pd=Project.objects.filter(dept='mech').filter(proj_category='PRODUCT DEVELOPMENT')
+    datamech_env=Project.objects.filter(dept='mech').filter(proj_category='ENVIRONMENT SUSTAINABILITY')
+    # data=Project.objects.all()
+
+    # print(datamech.values())
+    # print(datamech_rb.values())
+    # print(datamech_pd.values())
+    # print(datamech_env.values())
+    context={
+    'datamech':datamech,
+    'datamech_rb':datamech_rb,
+    'datamech_pd':datamech_pd,
+    'datamech_env':datamech_env
+    }
+    return render(request, "website1/mech.html",context)
 
 
 def extcdept(request):
-    return render(request, "website1/extc.html")
+    dataextc=Project.objects.filter(dept='extc')
+    dataextc_rb=Project.objects.filter(dept='extc').filter(proj_category='RESEARCH BASED')
+    dataextc_pd=Project.objects.filter(dept='extc').filter(proj_category='PRODUCT DEVELOPMENT')
+    dataextc_env=Project.objects.filter(dept='extc').filter(proj_category='ENVIRONMENT SUSTAINABILITY')
+    # data=Project.objects.all()
+
+    # print(dataextc.values())
+    # print(dataextc_rb.values())
+    # print(dataextc_pd.values())
+    # print(dataextc_env.values())
+    context={
+    'dataextc':dataextc,
+    'dataextc_rb':dataextc_rb,
+    'dataextc_pd':dataextc_pd,
+    'dataextc_env':dataextc_env
+    }
+    return render(request, "website1/extc.html",context)
 
 def dataentry(request):
     return render(request, "website1/dataentry.html")    
