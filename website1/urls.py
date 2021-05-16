9d99d982675244525a8a5ba5b5f799043678cd2c
Amish Thekke Parambil <amishtp147@outlook.com>
2021-05-16 21:31:07 +0530

    Update urls.py

--- website1/urls.py
+++ website1/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 
 from . import views
 
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
 	path('', views.home, name = 'home'),
@@ -12,3 +13,5 @@ urlpatterns = [
     path('privacypolicy', views.privacypolicy, name='privacypolicy'),
     path('editdata', views.edit_data, name='editdata')
 ]
+
+urlpatterns += staticfiles_urlpatterns()
